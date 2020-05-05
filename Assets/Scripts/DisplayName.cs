70f66282b912c6bd1f201a4af1210c061f9be383
HenrikBaumann <henrik_baumann@hotmail.com>
2020-05-05 18:48:21 +0200

    Task_1

---
+++ Assets/Scripts/DisplayName.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DisplayName : MonoBehaviour
+{
+    public String objectName;
+    public Text myText;
+    public float fadeTime;
+    public bool displayInfo;
+
+    private void Start()
+    {
+        myText = GameObject.Find("Text").GetComponent<Text>();
+        myText.color = Color.clear;
+    }
+
+    private void Update()
+    {
+        FadeText();
+    }
+
+    private void OnMouseOver()
+    {
+        displayInfo = true;
+    }
+
+    private void OnMouseExit()
+    {
+        displayInfo = false;
+    }
+    void FadeText()
+    {
+        if (displayInfo)
+        {
+            myText.text = objectName;
+            myText.color = Color.Lerp(myText.color, Color.white, fadeTime * Time.deltaTime);
+        }
+        else
+        {
+            myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+        }
+    }
+}
--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -1,18 +1,77 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class MouseManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Boolean primaryDown = false;
+    public Boolean secondaryDown = false;
+    public float speed = 500.0f;
+    private void Update()
     {
+        MouseInput(); //checks for Mouse clicks
+        MouseMovement(); //checks the Mouse movement
+        CamZoom();
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void MouseMovement()
     {
-        
+        if (Input.GetAxis("Mouse X") > 0 & secondaryDown == true)
+        {
+            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime*speed, 
+                0.0f, Input.GetAxisRaw("Mouse Y")*Time.deltaTime*speed);
+        }
+        else if (Input.GetAxis("Mouse X") < 0 & secondaryDown == true)
+        {
+            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime*speed, 
+                0.0f, Input.GetAxisRaw("Mouse Y")*Time.deltaTime*speed);
+        }
     }
+    void MouseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            primaryDown = true;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            secondaryDown = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            primaryDown = false;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            secondaryDown = false;
+        }
+    }
+
+    void CamZoom()
+    {
+        float ScrollWheelChange = Input.GetAxis("Mouse ScrollWheel");
+        if (ScrollWheelChange != 0)
+        {
+            float R = ScrollWheelChange * 15; 
+            float PosX = Camera.main.transform.eulerAngles.x + 90;
+            float PosY = -1 * (Camera.main.transform.eulerAngles.y - 90);
+            PosX = PosX / 180 * Mathf.PI; 
+            PosY = PosY / 180 * Mathf.PI;
+            float X = R * Mathf.Sin(PosX) * Mathf.Cos(PosY);
+            float Z = R * Mathf.Sin(PosX) * Mathf.Sin(PosY);
+            float Y = R * Mathf.Cos(PosX);
+            float CamX = Camera.main.transform.position.x;
+            float CamY = Camera.main.transform.position.y;
+            float CamZ = Camera.main.transform.position.z;
+            Camera.main.transform.position = new Vector3(CamX + X, CamY + Y, CamZ + Z);
+        }
+
+    }
+
 }
