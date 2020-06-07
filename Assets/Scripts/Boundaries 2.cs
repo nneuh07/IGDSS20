972b0fe10d56951674445e67ff4008d0c09dfacd
HenrikBaumann <henrik_baumann@hotmail.com>
2020-06-07 20:17:19 +0200

    First Push

---
+++ Assets/Scripts/Boundaries 2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    [SerializeField] private float x_max_limit;
+    [SerializeField] private float x_min_limit;
+    [SerializeField] private float y_max_limit;
+    [SerializeField] private float y_min_limit;
+    [SerializeField] private float z_min_limit;
+    [SerializeField] private float z_max_limit;
+
+    private void Update()
+    {
+        transform.position = new Vector3 //Vector for Clamping the Camera
+        (
+        Mathf.Clamp(transform.position.x , x_min_limit, x_max_limit),
+        Mathf.Clamp(transform.position.y, y_min_limit, y_max_limit),
+        Mathf.Clamp(transform.position.z, z_min_limit,z_max_limit)
+            );
+    }
+    
+}
--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 
 public class Building : MonoBehaviour
 {
-    [SerializeField] public String type;
-    [SerializeField] public int upkeep;
-    [SerializeField] public int buildCost;
-    [SerializeField] public int plankCost;
-    [SerializeField] public Tile tileReference;
-    [SerializeField] public int efficiencyValue;
-    [SerializeField] public int resourceInterval;
-    [SerializeField] public int output;
-    [SerializeField] public List<Tile> tiles;
-    [SerializeField] public int minNeighbours;
-    [SerializeField] public int maxNeighbours;
-    [SerializeField] public Resources inputResource;
-    [SerializeField] public Resources outputResource;
+    [SerializeField] public String type; //Type of Building
+    [SerializeField] public float upkeep; // Cost per minute
+    [SerializeField] public float buildCost; //Money for building
+    [SerializeField] public float plankCost; // Placement costs
+    [SerializeField] public Tile tileReference; // Reference to tile
+    [SerializeField] public float efficiencyValue = 1f; //Efficiency based on surrounding tiles 
+    [SerializeField] public float resourceInterval; //If operation on 100% efficiency
+    [SerializeField] public float output; //Number of output per cycle
+    [SerializeField] public List<Tile.TileTypes> tiles; //Which tiles it can be built on
+    [SerializeField] public float minNeighbours; //Minimum neighbours
+    [SerializeField] public float maxNeighbours; //MAx neighbour 
+    [SerializeField] public List<GameManager.ResourceTypes> inputResources = new List<GameManager.ResourceTypes>(); // Choice for input resources
+    [SerializeField] public GameManager.ResourceTypes outputResource;
+
+
 
     // Start is called before the first frame update
     void Start()
